dd04381424c1dba50ba4617d596496a4720e6e70
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-11-18 01:59:47 +0600

    3rd Problem solved

--- solution.ts
+++ solution.ts
@@ -1,18 +1,33 @@
-const formatValue=(value: string | number | boolean): string | number | boolean=>{
-    if(typeof value === "string"){
-        return value.toUpperCase();
-    }else if(typeof value === "number"){
-        return value*10;
-    }else{
-        return !value
-    }
+const formatValue = (
+  value: string | number | boolean
+): string | number | boolean => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
+};
+
+const getLength = (value: string | any[]): number => {
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value.length;
+  }
+};
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
 }
-
-
-const getLength = (value: string | any[]): number =>{
-    if(typeof value === "string"){
-        return value.length;
-    }else{
-        return value.length
-    }
-}  
